ab0c89b67d400ba534cbc72a3f3c28c2c350763e
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2026-02-17 00:11:57 +0200

    feat(loads): implemented ld [C] A and its opposite

--- src/cpu/cpu_context.rs
+++ src/cpu/cpu_context.rs
@@ -63,6 +63,16 @@ impl CpuContext {
                     let addr = 0xFF00 + self.fetch() as u16;
                     self.registers.a = self.memory.read(&mut self.clock, addr)?;
                 } // LDH A [A8]
+                0xE2 => {
+                    print!("ldh [C] a");
+                    let addr = 0xFF00 + self.registers.c as u16;
+                    self.memory.write(&mut self.clock, addr, self.registers.a)?;
+                } // LDH [C] A
+                0xF2 => {
+                    print!("ldh a [C]");
+                    let addr = 0xFF00 + self.registers.c as u16;
+                    self.registers.a = self.memory.read(&mut self.clock, addr)?;
+                } // LDH A [C]
                 0x8 => loads_16::ld_n16_sp(self)?, // LD [imm16] SP
                 0x06 | 0x16 | 0x26 | 0x36 | 0x0E | 0x1E | 0x2E | 0x3E | 0x40..0x80 => {
                     loads::load_r8(self, opcode)?
