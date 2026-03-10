4d3eec177d65b96871c47175ec4e94a1d427bf94
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2026-03-10 15:55:46 +0200

    fix: correctly set joypad register bit set

--- src/cpu/cpu_context.rs
+++ src/cpu/cpu_context.rs
@@ -63,7 +63,7 @@ impl CpuContext {
     }
 
     pub fn handle_joypad(&mut self, keycode: Keycode, is_down: bool) {
-        let mut joypad_reg = self.memory.io[0];
+        let mut joypad_reg = &mut self.memory.io[0];
         let bit = match keycode {
             Keycode::Return | Keycode::Down => Some(3),
             Keycode::C | Keycode::Up => Some(2),
@@ -72,8 +72,8 @@ impl CpuContext {
             _ => None,
         };
         if let Some(index) = bit {
-            joypad_reg = alu::set_bit(joypad_reg, index, !is_down);
-            if (alu::read_bits(joypad_reg, 4, 1) == 0 || alu::read_bits(joypad_reg, 5, 1) == 0)
+            *joypad_reg = alu::set_bit(*joypad_reg, index, !is_down);
+            if (alu::read_bits(*joypad_reg, 4, 1) == 0 || alu::read_bits(*joypad_reg, 5, 1) == 0)
                 && is_down
             {
                 self.memory.io[0x0F] = alu::set_bit(self.memory.io[0x0F], 4, true);
