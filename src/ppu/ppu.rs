625641189fdb3d71834d6caf56318998ee0d7314
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2026-03-14 20:20:43 +0200

    Revert "feat(ppu): H-Blank interrupt"
    This reverts commit 91691cb432d7c0d05ef51c9d2ac2ea9aaea0ed52.
    it caused too much issues for some reason

--- src/ppu/ppu.rs
+++ src/ppu/ppu.rs
@@ -37,8 +37,6 @@ impl PPU {
         if context.t_cycles.abs_diff(context.ppu.last_cycle) >= 456 {
             if context.memory.io[LY] < 144 {
                 let _ = PPU::draw_scanline(context);
-                context.memory.io[IF] = alu::set_bit(context.memory.io[IF], 1, true);
-                context.memory.io[STAT] = alu::set_bit(context.memory.io[STAT], 3, true);
             }
             context.memory.io[LY] = context.memory.io[LY].wrapping_add(1);
             context.ppu.last_cycle = context.t_cycles;
